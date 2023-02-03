7056d4055421d64082fbbb5d849c2929246efea9
rbverissimo <rverissimo.dev@gmail.com>
2023-02-03 18:02:34 -0300

    Little story in order to be able to manipulate variables

--- site.php
+++ site.php
@@ -7,6 +7,12 @@
       <title>Document</title>
 </head>
 <body>
-     <?php echo("Hello World") ?>     
+     <?php 
+
+     echo "There was once a man named George <br>";
+     echo "He was 70 years old <br>";
+     echo "He really like the color blue  <br>";
+     echo "But he didn't like being 70 <br>";
+     ?>     
 </body>
 </html>
